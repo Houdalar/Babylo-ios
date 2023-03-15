9e49966b42325476ed80790692351adec09292fa
Houdalar <houda.lariani@esprit.tn>
2023-03-15 17:31:33 +0100

    empty project

---
+++ Babylo/BabyloApp.swift
@@ -0,0 +1,20 @@
+//
+//  BabyloApp.swift
+//  Babylo
+//
+//  Created by houda lariani on 15/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct BabyloApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
