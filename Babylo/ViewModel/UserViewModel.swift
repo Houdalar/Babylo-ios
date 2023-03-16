1cb6722304e7f943cface01376d9c4734018e27b
Houdalar <houda.lariani@esprit.tn>
2023-03-16 06:34:44 +0100

    adding Alamofire

--- Babylo/ViewModel/UserViewModel.swift
+++ Babylo/ViewModel/UserViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+// Alamofire
+
+class SignUpViewModel: ObservableObject {
+    
+   /* func signUp(username: String, password: String, email: String, completion: @escaping (Result<String, Error>) -> Void) {
+        let url = "http://localhost:8080/user/signup"
+        let parameters: [String: Any] = [        "username": username,        "password": password,        "email": email    ]
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success:
+                completion(.success("Account activation link sent."))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }*/
+}
